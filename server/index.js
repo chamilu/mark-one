f72328d736a4e838a6fcbbeceb8ebc0b0e126ea0
Chamil Udayanga <chamiludayanga28@gmail.com>
2019-07-24 23:03:11 +0800

    Added prettier formatting for server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const keys = require("./config/keys"); // TODO: set to handle environment vars.
+const keys = require('./config/keys'); // TODO: set to handle environment vars.
 
 // Load mongoose models
-require("./models/User");
+require('./models/User');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.MONGODB_URL, { useNewUrlParser: true }, () => {
-  console.log("===== mongodb connected =====");
+    console.log('===== mongodb connected =====');
 });
 
 // middlewares
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send({ name: "chamil udayanga" });
+app.get('/', (req, res) => {
+    res.send({ name: 'chamil udayanga' });
 });
 
-app.post("/api/authenticate", (req, res) => {
-  // TODO: move this functionality to register api.
-  // this is just to test db connectivity and etc.
-  const User = mongoose.model("users");
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  }).save();
+app.post('/api/authenticate', (req, res) => {
+    // TODO: move this functionality to register api.
+    // this is just to test db connectivity and etc.
+    const User = mongoose.model('users');
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    }).save();
 
-  user.then(
-    data => {
-      res.send({ name: "chamil udayanga, this working", data });
-    },
-    err => {
-      res.send({ name: "issue occured" });
-    }
-  );
+    user.then(
+        data => {
+            res.send({ name: 'chamil udayanga, this working', data });
+        },
+        err => {
+            res.send({ name: 'issue occured' });
+        }
+    );
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("server started on", PORT);
+    console.log('server started on', PORT);
 });
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+    username: String,
+    password: String
 });
 
-mongoose.model("users", userSchema);
+mongoose.model('users', userSchema);
